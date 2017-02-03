GerbilSoft/TWLoader

commit b91fe3b3528c7c3c1a91a6eb3f151344dcef6572

Check for the TWLNAND-side title at the beginning and show the warning message at startup.
Keeping it in the main loop prevents it from hanging when exiting from
the HOME Menu.

gui/source/main.cpp
@@ -1441,15 +1441,29 @@ int main()
 	aptHook(&rfhm_cookie, rfhm_callback, &bannertextloaded);
 	
 	// Loop as long as the status is not exit
-	bool saveOnExit = true;
+	const bool isTWLNANDInstalled = checkTWLNANDSide();
+	// Save by default if the TWLNAND-side title is installed.
+	// Otherwise, we don't want to save anything.
+	bool saveOnExit = !isTWLNANDInstalled;
 	while(run && aptMainLoop()) {
 	//while(run) {
 		// Scan hid shared memory for input events
 		hidScanInput();
-		
+
 		const u32 hDown = hidKeysDown();
 		const u32 hHeld = hidKeysHeld();
-		
+
+		// Check if the TWLNAND-side title is installed.
+		if (!isTWLNANDInstalled) {
+			sf2d_start_frame(GFX_BOTTOM, GFX_LEFT);
+			sf2d_draw_texture(dialogboxtex, 0, 0);
+			sftd_draw_text(font, 12, 16, RGBA8(0, 0, 0, 255), 12, "Please, install TWLNand side and retry again");
+			sftd_draw_text(font, 12, 28, RGBA8(0, 0, 0, 255), 12, "Returning to Home...");
+			sf2d_end_frame();
+			sf2d_swapbuffers();
+			continue;
+		}
+
 		offset3D[0].topbg = CONFIG_3D_SLIDERSTATE * -12.0f;
 		offset3D[1].topbg = CONFIG_3D_SLIDERSTATE * 12.0f;
 		offset3D[0].boxart = CONFIG_3D_SLIDERSTATE * -5.0f;
@@ -3281,16 +3295,7 @@ int main()
 			SaveSettings();
 			SetPerGameSettings();
 			SaveBootstrapConfig();
-			
-			// Check if TWLNAND side is installed.
-			if(!checkTWLNANDSide()){
-				sf2d_start_frame(GFX_BOTTOM, GFX_LEFT);
-				sf2d_draw_texture(dialogboxtex, 0, 0);
-				sftd_draw_text(font, 12, 16, RGBA8(0, 0, 0, 255), 12, "Please, install TWLNand side and retry again");
-				sftd_draw_text(font, 12, 28, RGBA8(0, 0, 0, 255), 12, "Returning to Home...");
-				break;
-			}
-			
+
 			// Prepare for the app launch.
 			u64 tid = TWLNAND_TID;
 			FS_MediaType mediaType = MEDIATYPE_NAND;

commit e085c990dd34475f37f5ff4826ecbdd03c9955ee

saveOnExit should be true if the TWLNAND title is installed, not the other way around.
This fixes a regression from commit b91fe3b3528c7c3c1a91a6eb3f151344dcef6572.
(Check for the TWLNAND-side title at the beginning and show the warning message at startup.)

gui/source/main.cpp
@@ -1442,7 +1442,7 @@ int main()
 	const bool isTWLNANDInstalled = checkTWLNANDSide();
 	// Save by default if the TWLNAND-side title is installed.
 	// Otherwise, we don't want to save anything.
-	bool saveOnExit = !isTWLNANDInstalled;
+	bool saveOnExit = isTWLNANDInstalled;
 	while(run && aptMainLoop()) {
 	//while(run) {
 		// Scan hid shared memory for input events