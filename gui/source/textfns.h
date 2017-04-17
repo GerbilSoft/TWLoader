5dbb09097ff6c2c163442cc1c9b2e370d2848217
David Korth <gerbilsoft@gerbilsoft.com>
2017-04-16 20:27:30 -0400

    textfns.cpp: Use better variable names that indicate in vs. out and UTF-8 vs. UTF-16.

--- gui/source/textfns.h
+++ gui/source/textfns.h
@@ -8,52 +8,52 @@
 
 /**
  * Convert a UTF-16 string to wstring.
- * @param str UTF-16 string.
+ * @param in_u16str UTF-16 string.
  * @return wstring. (UTF-32)
  */
-std::wstring utf16_to_wstring(const u16 *str);
+std::wstring utf16_to_wstring(const u16 *in_u16str);
 
 /**
  * Convert a UTF-16 string to wchar_t*.
- * @param str UTF-16 string.
- * @return malloc()'d wchar_t*. (UTF-32) (NOTE: If str is nullptr, this returns nullptr.)
+ * @param in_u16str UTF-16 string.
+ * @return malloc()'d wchar_t*. (UTF-32) (NOTE: If in_u16str is nullptr, this returns nullptr.)
  */
-wchar_t *utf16_to_wchar(const u16 *str);
+wchar_t *utf16_to_wchar(const u16 *in_u16str);
 
 /**
  * Convert a UTF-16 string with newlines to a vector of wstrings.
- * @param str UTF-16 string with newlines. (not necessarily NULL-terminated)
- * @param len Length of str.
+ * @param in_u16str UTF-16 string with newlines. (not necessarily NULL-terminated)
+ * @param len Length of in_u16str.
  * @return vector<wstring>, split on newline boundaries.
  */
-std::vector<std::wstring> utf16_nl_to_vwstring(const u16 *str, int len);
+std::vector<std::wstring> utf16_nl_to_vwstring(const u16 *in_u16str, int len);
 
 /**
  * Convert a UTF-8 string to wstring.
- * @param str UTF-8 string.
+ * @param in_u8str UTF-8 string.
  * @return wstring. (UTF-32)
  */
-std::wstring utf8_to_wstring(const char *str);
+std::wstring utf8_to_wstring(const char *in_u8str);
 
 /**
  * Convert a UTF-8 string to wchar_t*.
- * @param str UTF-8 string.
- * @return malloc()'d wchar_t*. (UTF-32) (NOTE: If str is nullptr, this returns nullptr.)
+ * @param in_u8str UTF-8 string.
+ * @return malloc()'d wchar_t*. (UTF-32) (NOTE: If in_u8str is nullptr, this returns nullptr.)
  */
-wchar_t *utf8_to_wchar(const char *str);
+wchar_t *utf8_to_wchar(const char *in_u8str);
 
 /**
  * Convert a Latin-1 string to wstring.
- * @param str Latin-1 string.
+ * @param in_str Latin-1 string.
  * @return wstring. (UTF-32)
  */
-std::wstring latin1_to_wstring(const char *str);
+std::wstring latin1_to_wstring(const char *in_str);
 
 /**
  * Convert a Latin1- string to wchar_t*.
- * @param str Latin-1 string.
- * @return malloc()'d wchar_t*. (UTF-32) (NOTE: If str is nullptr, this returns nullptr.)
+ * @param in_str Latin-1 string.
+ * @return malloc()'d wchar_t*. (UTF-32) (NOTE: If in_str is nullptr, this returns nullptr.)
  */
-wchar_t *latin1_to_wchar(const char *str);
+wchar_t *latin1_to_wchar(const char *in_str);
 
 #endif /* TEXTFNS_H */
