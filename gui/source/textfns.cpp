ce2b866a8bb87b562732f1a46a56231a1275c20c
David Korth <gerbilsoft@gerbilsoft.com>
2017-04-16 20:27:30 -0400

    textfns.cpp: Use better variable names that indicate in vs. out and UTF-8 vs. UTF-16.

--- gui/source/textfns.cpp
+++ gui/source/textfns.cpp
@@ -13,50 +13,50 @@ using std::wstring;
 
 /**
  * Convert a UTF-16 string to wchar_t. (internal function
- * @param wstr	[out] UTF-32 buffer.
- * @param str	[in] UTF-16 string.
+ * @param out_wstr	[out] UTF-32 buffer.
+ * @param in_u16str	[in] UTF-16 string.
  * @return Number of characters used in wstr, not including the NULL terminator.
  */
-static int utf16_to_wchar_internal(wchar_t *wstr, const u16 *str)
+static int utf16_to_wchar_internal(wchar_t *out_wstr, const u16 *in_u16str)
 {
-	if (!str) {
+	if (!in_u16str) {
 		// No string.
-		*wstr = 0;
+		*out_wstr = 0;
 		return 0;
 	}
 
 	int size = 0;
-	for (; *str != 0; str++, wstr++, size++) {
+	for (; *in_u16str != 0; in_u16str++, out_wstr++, size++) {
 		// Convert the UTF-16 character to UTF-32.
 		// Special handling is needed only for surrogate pairs.
 		// (TODO: Test surrogate pair handling.)
-		if ((*str & 0xFC00) == 0xD800) {
+		if ((*in_u16str & 0xFC00) == 0xD800) {
 			// High surrogate. (0xD800-0xDBFF)
-			if ((str[1] & 0xFC00) == 0xDC00) {
+			if ((in_u16str[1] & 0xFC00) == 0xDC00) {
 				// Low surrogate. (0xDC00-0xDFFF)
 				// Recombine to get the actual character.
 				wchar_t wchr = 0x10000;
-				wchr += ((str[0] & 0x3FF) << 10);
-				wchr +=  (str[1] & 0x3FF);
-				*wstr = wchr;
+				wchr += ((in_u16str[0] & 0x3FF) << 10);
+				wchr +=  (in_u16str[1] & 0x3FF);
+				*out_wstr = wchr;
 				// Make sure we don't process the low surrogate
 				// on the next iteration.
-				str++;
+				in_u16str++;
 			} else {
 				// Unpaired high surrogate.
-				*wstr = (wchar_t)(0xFFFD);
+				*out_wstr = (wchar_t)(0xFFFD);
 			}
-		} else if ((*str & 0xFC00) == 0xDC00) {
+		} else if ((*in_u16str & 0xFC00) == 0xDC00) {
 			// Unpaired low surrogate.
-			*wstr = (wchar_t)(0xFFFD);
+			*out_wstr = (wchar_t)(0xFFFD);
 		} else {
 			// Standard UTF-16 character.
-			*wstr = (wchar_t)*str;
+			*out_wstr = (wchar_t)*in_u16str;
 		}
 	}
 
 	// Add a NULL terminator.
-	*wstr = 0;
+	*out_wstr = 0;
 	return size;
 }
 
@@ -65,10 +65,10 @@ static int utf16_to_wchar_internal(wchar_t *wstr, const u16 *str)
  * @param str UTF-16 string.
  * @return wstring. (UTF-32)
  */
-wstring utf16_to_wstring(const u16 *str)
+wstring utf16_to_wstring(const u16 *in_u16str)
 {
 	wstring wstr;
-	if (!str) {
+	if (!in_u16str) {
 		// No string.
 		return wstr;
 	}
@@ -77,11 +77,11 @@ wstring utf16_to_wstring(const u16 *str)
 	// as there are characters in the string,
 	// plus one for the NULL terminator.
 	int len = 0;
-	for (const u16 *p = str; *p != 0; p++, len++) { }
+	for (const u16 *p = in_u16str; *p != 0; p++, len++) { }
 	wstr.resize(len+1);
 
 	// Convert the string.
-	int size = utf16_to_wchar_internal(&wstr[0], str);
+	int size = utf16_to_wchar_internal(&wstr[0], in_u16str);
 
 	// Resize the string to trim extra spaces.
 	wstr.resize(size);
@@ -90,11 +90,11 @@ wstring utf16_to_wstring(const u16 *str)
 
 /**
  * Convert a UTF-16 string with newlines to a vector of wstrings.
- * @param str UTF-16 string with newlines.
- * @param len Length of str.
+ * @param in_u16str UTF-16 string with newlines.
+ * @param len Length of in_u16str.
  * @return vector<wstring>, split on newline boundaries.
  */
-vector<wstring> utf16_nl_to_vwstring(const u16 *str, int len)
+vector<wstring> utf16_nl_to_vwstring(const u16 *in_u16str, int len)
 {
 	// Buffers for the strings.
 	// Assuming wchar_t is 32-bit.
@@ -104,33 +104,33 @@ vector<wstring> utf16_nl_to_vwstring(const u16 *str, int len)
 	wstring wstr;
 	wstr.reserve(64);
 
-	for (; *str != 0 && len > 0; str++, len--) {
+	for (; *in_u16str != 0 && len > 0; in_u16str++, len--) {
 		// Convert the UTF-16 character to UTF-32.
 		// Special handling is needed only for surrogate pairs.
 		// (TODO: Test surrogate pair handling.)
-		if ((*str & 0xFC00) == 0xD800) {
+		if ((*in_u16str & 0xFC00) == 0xD800) {
 			// High surrogate. (0xD800-0xDBFF)
-			if (len > 2 && (str[1] & 0xFC00) == 0xDC00) {
+			if (len > 2 && (in_u16str[1] & 0xFC00) == 0xDC00) {
 				// Low surrogate. (0xDC00-0xDFFF)
 				// Recombine to get the actual character.
 				wchar_t wchr = 0x10000;
-				wchr += ((str[0] & 0x3FF) << 10);
-				wchr +=  (str[1] & 0x3FF);
+				wchr += ((in_u16str[0] & 0x3FF) << 10);
+				wchr +=  (in_u16str[1] & 0x3FF);
 				wstr += wchr;
 				// Make sure we don't process the low surrogate
 				// on the next iteration.
-				str++;
+				in_u16str++;
 				len--;
 			} else {
 				// Unpaired high surrogate.
 				wstr += (wchar_t)(0xFFFD);
 			}
-		} else if ((*str & 0xFC00) == 0xDC00) {
+		} else if ((*in_u16str & 0xFC00) == 0xDC00) {
 			// Unpaired low surrogate.
 			wstr += (wchar_t)(0xFFFD);
 		} else {
 			// Standard UTF-16 character.
-			switch (*str) {
+			switch (*in_u16str) {
 				case L'\r':
 					// Skip carriage returns.
 					break;
@@ -141,7 +141,7 @@ vector<wstring> utf16_nl_to_vwstring(const u16 *str, int len)
 					break;
 				default:
 					// Add the character.
-					wstr += *str;
+					wstr += *in_u16str;
 					break;
 			}
 		}
@@ -157,12 +157,12 @@ vector<wstring> utf16_nl_to_vwstring(const u16 *str, int len)
 
 /**
  * Convert a UTF-16 string to wchar_t*.
- * @param str UTF-16 string.
- * @return malloc()'d wchar_t*. (UTF-32) (NOTE: If str is nullptr, this returns nullptr.)
+ * @param in_u16str UTF-16 string.
+ * @return malloc()'d wchar_t*. (UTF-32) (NOTE: If in_u16str is nullptr, this returns nullptr.)
  */
-wchar_t *utf16_to_wchar(const u16 *str)
+wchar_t *utf16_to_wchar(const u16 *in_u16str)
 {
-	if (!str) {
+	if (!in_u16str) {
 		// No string.
 		return nullptr;
 	}
@@ -171,11 +171,11 @@ wchar_t *utf16_to_wchar(const u16 *str)
 	// as there are characters in the string,
 	// plus one for the NULL terminator.
 	int len = 0;
-	for (const u16 *p = str; *p != 0; p++, len++) { }
+	for (const u16 *p = in_u16str; *p != 0; p++, len++) { }
 	wchar_t *wstr = (wchar_t*)malloc((len+1) * sizeof(wchar_t));
 
 	// Convert the string.
-	utf16_to_wchar_internal(wstr, str);
+	utf16_to_wchar_internal(wstr, in_u16str);
 	return wstr;
 }
 
@@ -183,84 +183,84 @@ wchar_t *utf16_to_wchar(const u16 *str)
 
 /**
  * Convert a UTF-8 string to wchar_t. (internal function
- * @param wstr	[out] UTF-32 buffer.
- * @param str	[in] UTF-8 string.
+ * @param out_wstr	[out] UTF-32 buffer.
+ * @param in_u8str	[in] UTF-8 string.
  * @return Number of characters used in wstr, not including the NULL terminator.
  */
-static int utf8_to_wchar_internal(wchar_t *wstr, const char *str)
+static int utf8_to_wchar_internal(wchar_t *out_wstr, const char *in_u8str)
 {
-	if (!str) {
+	if (!in_u8str) {
 		// No string.
-		*wstr = 0;
+		*out_wstr = 0;
 		return 0;
 	}
 
 	int size = 0;
-	for (; *str != 0; str++, wstr++, size++) {
-		if (!(*str & 0x80)) {
+	for (; *in_u8str != 0; in_u8str++, out_wstr++, size++) {
+		if (!(*in_u8str & 0x80)) {
 			// ASCII. (U+0000-U+007F)
-			*wstr = *str;
-		} else if ((*str & 0xE0) == 0xC0) {
+			*out_wstr = *in_u8str;
+		} else if ((*in_u8str & 0xE0) == 0xC0) {
 			// Two-byte UTF-8.
-			if ((str[1] & 0xC0) != 0x80) {
+			if ((in_u8str[1] & 0xC0) != 0x80) {
 				// Invalid sequence.
-				*wstr = (wchar_t)0xFFFD;
+				*out_wstr = (wchar_t)0xFFFD;
 			} else {
-				*wstr = (wchar_t)(
-					((str[0] & 0x1F) << 6) |
-					((str[1] & 0x3F)));
-				str++;
+				*out_wstr = (wchar_t)(
+					((in_u8str[0] & 0x1F) << 6) |
+					((in_u8str[1] & 0x3F)));
+				in_u8str++;
 			}
-		} else if ((*str & 0xF0) == 0xE0) {
+		} else if ((*in_u8str & 0xF0) == 0xE0) {
 			// Three-byte UTF-8.
-			if ((str[1] & 0xC0) != 0x80 ||
-			    (str[2] & 0xC0) != 0x80)
+			if ((in_u8str[1] & 0xC0) != 0x80 ||
+			    (in_u8str[2] & 0xC0) != 0x80)
 			{
 				// Invalid sequence.
-				*wstr = (wchar_t)0xFFFD;
+				*out_wstr = (wchar_t)0xFFFD;
 			} else {
-				*wstr = (wchar_t)(
-					((str[0] & 0x0F) << 12) |
-					((str[1] & 0x3F) << 6)  |
-					((str[2] & 0x3F)));
-				str += 2;
+				*out_wstr = (wchar_t)(
+					((in_u8str[0] & 0x0F) << 12) |
+					((in_u8str[1] & 0x3F) << 6)  |
+					((in_u8str[2] & 0x3F)));
+				in_u8str += 2;
 			}
-		} else if ((*str & 0xF8) == 0xF0) {
+		} else if ((*in_u8str & 0xF8) == 0xF0) {
 			// Four-byte UTF-8.
-			if ((str[1] & 0xC0) != 0x80 ||
-			    (str[2] & 0xC0) != 0x80 ||
-			    (str[3] & 0xC0) != 0x80)
+			if ((in_u8str[1] & 0xC0) != 0x80 ||
+			    (in_u8str[2] & 0xC0) != 0x80 ||
+			    (in_u8str[3] & 0xC0) != 0x80)
 			{
 				// Invalid sequence.
-				*wstr = (wchar_t)0xFFFD;
+				*out_wstr = (wchar_t)0xFFFD;
 			} else {
-				*wstr = (wchar_t)(
-					((str[0] & 0x07) << 18) |
-					((str[1] & 0x3F) << 12) |
-					((str[2] & 0x3F) << 6)  |
-					((str[3] & 0x3F)));
-				str += 3;
+				*out_wstr = (wchar_t)(
+					((in_u8str[0] & 0x07) << 18) |
+					((in_u8str[1] & 0x3F) << 12) |
+					((in_u8str[2] & 0x3F) << 6)  |
+					((in_u8str[3] & 0x3F)));
+				in_u8str += 3;
 			}
 		} else {
 			// Invalid UTF-8 sequence.
-			*wstr = (wchar_t)0xFFFD;
+			*out_wstr = (wchar_t)0xFFFD;
 		}
 	}
 
 	// Add a NULL terminator.
-	*wstr = 0;
+	*out_wstr = 0;
 	return size;
 }
 
 /**
  * Convert a UTF-8 string to wstring.
- * @param str UTF-8 string.
+ * @param in_u8str UTF-8 string.
  * @return wstring. (UTF-32)
  */
-wstring utf8_to_wstring(const char *str)
+wstring utf8_to_wstring(const char *in_u8str)
 {
 	wstring wstr;
-	if (!str) {
+	if (!in_u8str) {
 		// No string.
 		return wstr;
 	}
@@ -268,10 +268,10 @@ wstring utf8_to_wstring(const char *str)
 	// Allocate at least as many UTF-32 units
 	// as there are bytes in the string, plus
 	// one for the NULL terminator.
-	wstr.resize(strlen(str)+1);
+	wstr.resize(strlen(in_u8str)+1);
 
 	// Convert the string.
-	int size = utf8_to_wchar_internal(&wstr[0], str);
+	int size = utf8_to_wchar_internal(&wstr[0], in_u8str);
 
 	// Resize the string to trim extra spaces.
 	wstr.resize(size);
@@ -280,12 +280,12 @@ wstring utf8_to_wstring(const char *str)
 
 /**
  * Convert a UTF-8 string to wchar_t*.
- * @param str UTF-8 string.
- * @return malloc()'d wchar_t*. (UTF-32) (NOTE: If str is nullptr, this returns nullptr.)
+ * @param in_u8str UTF-8 string.
+ * @return malloc()'d wchar_t*. (UTF-32) (NOTE: If in_u8str is nullptr, this returns nullptr.)
  */
-wchar_t *utf8_to_wchar(const char *str)
+wchar_t *utf8_to_wchar(const char *in_u8str)
 {
-	if (!str) {
+	if (!in_u8str) {
 		// No string.
 		return nullptr;
 	}
@@ -293,10 +293,10 @@ wchar_t *utf8_to_wchar(const char *str)
 	// Allocate at least as many UTF-32 units
 	// as there are bytes in the string, plus
 	// one for the NULL terminator.
-	wchar_t *wstr = (wchar_t*)malloc((strlen(str)+1) * sizeof(wchar_t));
+	wchar_t *wstr = (wchar_t*)malloc((strlen(in_u8str)+1) * sizeof(wchar_t));
 
 	// Convert the string.
-	utf8_to_wchar_internal(wstr, str);
+	utf8_to_wchar_internal(wstr, in_u8str);
 	return wstr;
 }
 
@@ -304,37 +304,37 @@ wchar_t *utf8_to_wchar(const char *str)
 
 /**
  * Convert a Latin-1 string to wchar_t. (internal function
- * @param wstr	[out] UTF-32 buffer.
- * @param str	[in] Latin-1 string.
+ * @param out_wstr	[out] UTF-32 buffer.
+ * @param in_str	[in] Latin-1 string.
  * @return Number of characters used in wstr, not including the NULL terminator.
  */
-static int latin1_to_wchar_internal(wchar_t *wstr, const char *str)
+static int latin1_to_wchar_internal(wchar_t *out_wstr, const char *in_str)
 {
-	if (!str) {
+	if (!in_str) {
 		// No string.
-		*wstr = 0;
+		*out_wstr = 0;
 		return 0;
 	}
 
 	int size = 0;
-	for (; *str != 0; str++, wstr++, size++) {
-		*wstr = *str;
+	for (; *in_str != 0; in_str++, out_wstr++, size++) {
+		*out_wstr = *in_str;
 	}
 
 	// Add a NULL terminator.
-	*wstr = 0;
+	*out_wstr = 0;
 	return size;
 }
 
 /**
  * Convert a Latin-1 string to wstring.
- * @param str Latin-1 string.
+ * @param in_str Latin-1 string.
  * @return wstring. (UTF-32)
  */
-wstring latin1_to_wstring(const char *str)
+wstring latin1_to_wstring(const char *in_str)
 {
 	wstring wstr;
-	if (!str) {
+	if (!in_str) {
 		// No string.
 		return wstr;
 	}
@@ -342,10 +342,10 @@ wstring latin1_to_wstring(const char *str)
 	// Allocate at least as many UTF-32 units
 	// as there are bytes in the string, plus
 	// one for the NULL terminator.
-	wstr.resize(strlen(str)+1);
+	wstr.resize(strlen(in_str)+1);
 
 	// Convert the string.
-	int size = latin1_to_wchar_internal(&wstr[0], str);
+	int size = latin1_to_wchar_internal(&wstr[0], in_str);
 
 	// Resize the string to trim extra spaces.
 	wstr.resize(size);
@@ -354,12 +354,12 @@ wstring latin1_to_wstring(const char *str)
 
 /**
  * Convert a Latin-1 string to wchar_t*.
- * @param str Latin-1 string.
- * @return malloc()'d wchar_t*. (UTF-32) (NOTE: If str is nullptr, this returns nullptr.)
+ * @param in_str Latin-1 string.
+ * @return malloc()'d wchar_t*. (UTF-32) (NOTE: If in_str is nullptr, this returns nullptr.)
  */
-wchar_t *latin1_to_wchar(const char *str)
+wchar_t *latin1_to_wchar(const char *in_str)
 {
-	if (!str) {
+	if (!in_str) {
 		// No string.
 		return nullptr;
 	}
@@ -367,9 +367,9 @@ wchar_t *latin1_to_wchar(const char *str)
 	// Allocate at least as many UTF-32 units
 	// as there are bytes in the string, plus
 	// one for the NULL terminator.
-	wchar_t *wstr = (wchar_t*)malloc((strlen(str)+1) * sizeof(wchar_t));
+	wchar_t *wstr = (wchar_t*)malloc((strlen(in_str)+1) * sizeof(wchar_t));
 
 	// Convert the string.
-	latin1_to_wchar_internal(wstr, str);
+	latin1_to_wchar_internal(wstr, in_str);
 	return wstr;
 }
